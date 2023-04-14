7954b3e1bd9d0ab5f2efd29fa073802f6ffd0e9b
Ignacio Presas <ipmain07@gmail.com>
2023-04-14 10:35:48 -0300

    add: exercise 4 solved

---
+++ exercises/problem_4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float side_1, side_2, side_3;
+
+    printf("Enter the size of the sides of a triangle (Use spaces to separate them): ");
+    scanf("%f %f %f", &side_1, &side_2, &side_3);
+
+    if ((side_1 + side_2) > side_3 || (side_1 + side_3) > side_2 || (side_2 + side_3) > side_1)
+    {
+        if (side_1 == side_2 && side_1 == side_3 && side_2 == side_3)
+        {
+            printf("Your triangle is Equilateral!");
+        }
+        else if (side_1 != side_2 && side_1 != side_3 && side_2 != side_3)
+        {
+            printf("Your triangle is Scalene!");
+        }
+        else
+        {
+            printf("Your triangle is Isosceles!");
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Error: The sides entered do not form a valid triangle");
+
+        return 1;
+    }
+
+    return 0;
+}
